62b32761dabad2dc99453bb2cfa83ec81ef68962
nicolasult <nicolas.ultrillo@gmail.com>
2025-08-15 20:11:19 +0200

    background setup

---
+++ effects/space_background.gd
@@ -0,0 +1,8 @@
+extends ParallaxBackground
+
+@onready var space_layer: ParallaxLayer = $SpaceLayer
+@onready var far_stars_layer: ParallaxLayer = $FarStarsLayer
+@onready var close_starts_layer: ParallaxLayer = $CloseStartsLayer
+
+func _process(delta: float) -> void:
+	pass
