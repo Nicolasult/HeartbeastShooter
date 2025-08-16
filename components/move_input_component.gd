314b943a57f4f0534dd2d90966dc3df98fc94b46
nicolasult <nicolas.ultrillo@gmail.com>
2025-08-16 12:03:02 +0200

    hitbox and hurtbox setup

--- components/move_input_component.gd
+++ components/move_input_component.gd
@@ -4,6 +4,6 @@ extends Node
 @export var move_stats: MoveStats
 @export var move_component: MoveComponent
 
-func _input(event) -> void:
+func _input(_event) -> void:
 	var input_axis = Input.get_axis("ui_left", "ui_right")
 	move_component.velocity = Vector2(input_axis * move_stats.speed, 0)
--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -6,6 +6,11 @@ extends Node2D
 @onready var scale_component: ScaleComponent = $ScaleComponent
 @onready var flash_component: FlashComponent = $FlashComponent
 @onready var shake_component: ShakeComponent = $ShakeComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
 
 func _ready() -> void:
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+	hurtbox_component.hurt.connect(func(_hitbox: HitboxComponent):
+		queue_free()
+		)
