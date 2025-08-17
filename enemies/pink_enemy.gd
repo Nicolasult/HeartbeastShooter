a53b569f56d8a11e804cfe79e72cf75e6eb681e0
nicolasult <nicolas.ultrillo@gmail.com>
2025-08-17 23:08:21 +0200

    update pink enemy so it can shoot according to its state

--- enemies/pink_enemy.gd
+++ enemies/pink_enemy.gd
@@ -7,6 +7,8 @@ extends Enemy
 @onready var move_side_state: TimedStateComponent = %MoveSideState
 @onready var pause_state: TimedStateComponent = %PauseState
 @onready var move_side_component: MoveComponent = $States/MoveSideState/MoveSideComponent
+@onready var fire_state: StateComponent = %FireState
+@onready var projectile_spawner_component: SpawnerComponent = %ProjectileSpawnerComponent
 
 
 func _ready() -> void:
@@ -18,7 +20,14 @@ func _ready() -> void:
 	move_side_component.velocity.x = [-20, 20].pick_random()
 	
 	move_down_state.state_finished.connect(move_side_state.enable)
-	move_side_state.state_finished.connect(pause_state.enable)
+	move_side_state.state_finished.connect(func():
+		fire_state.enable()
+		scale_component.tween_scale()
+		projectile_spawner_component.spawn(global_position)
+		fire_state.disable()
+		fire_state.state_finished.emit()
+		)
+	fire_state.state_finished.connect(pause_state.enable)
 	pause_state.state_finished.connect(move_down_state.enable)
 	
 	move_down_state.enable()
