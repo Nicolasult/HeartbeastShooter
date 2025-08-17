03d6fbbace645beb375f73d165191c624bf70c94
nicolasult <nicolas.ultrillo@gmail.com>
2025-08-17 22:51:51 +0200

    loops states of the pink enemy

--- enemies/pink_enemy.gd
+++ enemies/pink_enemy.gd
@@ -13,6 +13,12 @@ func _ready() -> void:
 	super()
 	
 	for state in states.get_children():
-		state.dispable()
+		state.disable()
 	
 	move_side_component.velocity.x = [-20, 20].pick_random()
+	
+	move_down_state.state_finished.connect(move_side_state.enable)
+	move_side_state.state_finished.connect(pause_state.enable)
+	pause_state.state_finished.connect(move_down_state.enable)
+	
+	move_down_state.enable()
