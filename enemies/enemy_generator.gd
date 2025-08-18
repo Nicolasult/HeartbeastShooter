305c77a9f42dbdb953f6a2126433e19aa750c8e6
nicolasult <nicolas.ultrillo@gmail.com>
2025-08-18 22:55:24 +0200

    increasing difficulty setup

--- enemies/enemy_generator.gd
+++ enemies/enemy_generator.gd
@@ -10,6 +10,8 @@ extends Node2D
 @onready var yellow_enemy_spawn_timer: Timer = $YellowEnemySpawnTimer
 @onready var pink_enemy_spawn_timer: Timer = $PinkEnemySpawnTimer
 
+@export var game_stats: GameStats
+
 var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
 var margin = 8
 
@@ -18,7 +20,8 @@ func _ready() -> void:
 	yellow_enemy_spawn_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene, yellow_enemy_spawn_timer))
 	pink_enemy_spawn_timer.timeout.connect(handle_spawn.bind(PinkEnemyScene, pink_enemy_spawn_timer))
 
-func handle_spawn(enemy_scene: PackedScene, timer: Timer) -> void:
+func handle_spawn(enemy_scene: PackedScene, timer: Timer, time_offset: float = 1.0) -> void:
 	spawner_component.scene = enemy_scene
 	spawner_component.spawn(Vector2(randf_range(margin, screen_width - margin), -16))
-	timer.start()
+	var spawn_rate = time_offset / (0.5 + (game_stats.score * 0.01))
+	timer.start(spawn_rate + randf_range(0.25, 0.5))
