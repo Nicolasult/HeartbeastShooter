8a41cb2479949763540e2e36610180a883e33b01
nicolasult <nicolas.ultrillo@gmail.com>
2025-08-19 19:19:33 +0200

    load highscore setup

--- menus/game_over.gd
+++ menus/game_over.gd
@@ -1,9 +1,14 @@
 extends Control
 
+const SAVE_PATH = "user://save.cfg"
+const TEST_SAVE_PATH = "res://save.cfg"
+
 @export var game_stats: GameStats
 
 @onready var score_value: Label = %ScoreValue
 @onready var highscore_value: Label = %HighscoreValue
+
+var save_path = TEST_SAVE_PATH
 
 func _ready() -> void:
 	if game_stats.score > game_stats.highscore:
@@ -15,3 +20,12 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		game_stats.score = 0
 		get_tree().change_scene_to_file("res://menus/menu.tscn")
+
+func load_highscore() -> void:
+	var config = ConfigFile.new()
+	var error = config.load(save_path)
+	if error != OK: return
+	game_stats.highscore = config.get_value("game", "highscore")
+	
+func save_highscore() -> void:
+	pass
