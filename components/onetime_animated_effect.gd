3788eda660835b37e2124a1146c811714db1d954
nicolasult <nicolas.ultrillo@gmail.com>
2025-08-19 18:53:10 +0200

    add sounds

--- components/onetime_animated_effect.gd
+++ components/onetime_animated_effect.gd
@@ -7,5 +7,6 @@ extends AnimatedSprite2D
 
 func _ready() -> void:
 	# Free this node when the animation is finished
+	
 	animation_finished.connect(queue_free)
 	animation_looped.connect(queue_free)
